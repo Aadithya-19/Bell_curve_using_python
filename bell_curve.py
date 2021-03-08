71c3cde3ffaf297194228a87e8d9ab62c38e9c33
AVioli <68108339+Aadithya-19@users.noreply.github.com>
2021-03-08 09:23:33 +0530

    Add files via upload

---
+++ bell_curve.py
@@ -0,0 +1,9 @@
+import plotly.figure_factory as ff
+import pandas as pd
+import csv
+
+df = pd.read_csv("data_1_bell.csv")
+
+fig = ff.create_distplot([df["Avg Rating"].tolist()], ["Avg Rating"])
+
+fig.show()
